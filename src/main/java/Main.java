afc65552913e33a7aa69809e764854efdbd90b8e
Aizuked <aizyk.tt@gmail.com>
2022-10-17 00:40:25 +0400

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,34 @@ import Jama.Matrix;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
 
-    static final double[][] mainMatrix = {
+    static final double[][] a = {
             {4.003, 0.207, 0.519, 0.281},
             {0.416, 3.273, 0.326, 0.375},
             {0.297, 0.351, 2.997, 0.429},
-            {0.412, 0.194, 0.215, 3.628}};
-    static final double[] results = {0.425, 0.021, 0.213, 0.946};
+            {0.412, 0.194, 0.215, 3.628}
+    };
+    static final double[][] d = {
+            {4.003, 0, 0, 0},
+            {0, 3.273, 0, 0},
+            {0, 0, 2.997, 0},
+            {0, 0, 0, 3.628}
+    };
+    static final double[][] l = {
+            {0, 0, 0, 0},
+            {0.416, 0, 0.326, 0},
+            {0.297, 0.351, 0, 0},
+            {0.412, 0.194, 0.215, 0}
+    };
+    static final double[][] r = {
+            {0, 0.207, 0.519, 0.281},
+            {0, 0, 0.326, 0.375},
+            {0, 0, 0, 0.429},
+            {0, 0, 0, 0}
+    };
+    static final double[] b = {0.425, 0.021, 0.213, 0.946};
 
     public static void main(String[] args) {
         BufferedReader bfReader =
@@ -27,11 +43,12 @@ public class Main {
             e.printStackTrace();
         }
 
-        Matrix A = new Matrix(mainMatrix);
-        int i = 0;
-        Matrix L = new Matrix(Arrays.stream(mainMatrix)
-                .map(j -> {
-                    i ==
-                }));
+        Matrix A = new Matrix(a);
+        Matrix D = new Matrix(d);
+        Matrix L = new Matrix(l);
+        Matrix R = new Matrix(r);
+        Matrix B = D.inverse().uminus().times(L.plus(R));
+        Matrix c = D.inverse().times(new Matrix(b, 1));
+
     }
 }
