e6369f9f8d146ac78b7cb3e3b44a09c475911d92
Aizuked <aizyk.tt@gmail.com>
2022-10-17 16:46:26 +0400

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,11 @@ import Jama.Matrix;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main {
 
-    static final double[][] a = {
-            {4.003, 0.207, 0.519, 0.281},
-            {0.416, 3.273, 0.326, 0.375},
-            {0.297, 0.351, 2.997, 0.429},
-            {0.412, 0.194, 0.215, 3.628}
-    };
     static final double[][] d = {
             {4.003, 0, 0, 0},
             {0, 3.273, 0, 0},
@@ -19,7 +15,7 @@ public class Main {
     };
     static final double[][] l = {
             {0, 0, 0, 0},
-            {0.416, 0, 0.326, 0},
+            {0.416, 0, 0, 0},
             {0.297, 0.351, 0, 0},
             {0.412, 0.194, 0.215, 0}
     };
@@ -34,21 +30,34 @@ public class Main {
     public static void main(String[] args) {
         BufferedReader bfReader =
                 new BufferedReader(new InputStreamReader(System.in));
-        int accuracy = 0;
+        double accuracy = 0;
         try {
-            accuracy = bfReader
-                    .readLine()
-                    .length() - 1;
+            accuracy = Double.parseDouble(bfReader.readLine());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        Matrix A = new Matrix(a);
         Matrix D = new Matrix(d);
         Matrix L = new Matrix(l);
         Matrix R = new Matrix(r);
         Matrix B = D.inverse().uminus().times(L.plus(R));
-        Matrix c = D.inverse().times(new Matrix(b, 1));
+        Matrix c = new Matrix(b, 1).times(D.inverse());
 
+        LinkedList<Matrix> cache = new LinkedList<>();
+        LinkedList<Double> deltaX = new LinkedList<>();
+
+        cache.addFirst(new Matrix(new double[]{0, 0, 0, 0}, 1));
+        deltaX.addFirst(0d);
+        cache.add(c);
+        deltaX.add(cache.getLast().minus(cache.get(cache.indexOf(cache.getLast()) - 1)).norm1());
+
+        while (cache.getLast().minus(cache.get(cache.indexOf(cache.getLast()) - 1)).norm1() >= (1 - B.norm1()) / B.norm1() * accuracy) {
+            cache.addLast(cache.getLast().times(B).plus(c));
+            deltaX.add(cache.getLast().minus(cache.get(cache.indexOf(cache.getLast()) - 1)).norm1());
+        }
+
+        cache.stream().forEachOrdered(i -> {
+            System.out.println(Arrays.deepToString(i.getArray()) + " " + deltaX.get(cache.indexOf(i)));
+        });
     }
 }
